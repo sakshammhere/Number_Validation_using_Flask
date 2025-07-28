166839785b69d0f19adfa8702d1824f62d7417cb
sakshammhere <guptasaksham0301@gmail.com>
2025-07-28 13:27:46 +0530

    Flask, DB Storage and Display

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for
+import requests
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Numbers_data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
-class Number_validation(db.Model): #thru a class, we define a schema for the database
+api_key="C92F0A5B17794E88A8C837331C0BCFAB"
+
+
+class Number_validation(db.Model): #thru a class we create a schema for our database
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(13), nullable=False)
     is_valid = db.Column(db.Boolean, nullable=False)
+    country_name = db.Column(db.String(50))
+    carrier = db.Column(db.String(50))
+    line_type = db.Column(db.String(50))
 
     def __repr__(self):
         return f'<Number {self.phone_number} - Valid: {self.is_valid}>'
 
+
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
+    result=None
     if request.method == "POST":
         phone_number = request.form.get("phone_number")
         if phone_number:
-            Numbers_data = Number_validation(phone_number=phone_number, is_valid=True)
-            db.session.add(Numbers_data)
+            url = f"https://api.veriphone.io/v2/verify?phone={phone_number}&key={api_key}"
+            response = requests.get(url)
+            data= response.json()
+
+            is_valid=data.get("phone_valid") 
+            if is_valid is None:
+                return "Invalid API Response/phone number.", 400  # Default to False if not present
+            
+            new_entry=Number_validation(
+                    phone_number=phone_number,
+                    is_valid=data.get("phone_valid"),
+                    country_name=data.get("country"),
+                    carrier=data.get("carrier"), 
+                    line_type=data.get("line_type") or "unknown"
+                )     
+            db.session.add(new_entry)
             db.session.commit()
-            return redirect(url_for("hello_world"))
+            result=new_entry
+            #return redirect(url_for("hello_world"))
 
-    # For GET requests or if phone_number not provided, simply render the template
-    return render_template("index.html")
-
-
+    # render template with result/and even if no phn number is entered
+    return render_template("index.html",result=result)
 
 
 @app.route("/products")
